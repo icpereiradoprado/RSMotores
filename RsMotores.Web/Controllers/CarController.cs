7fdfe2b632152eb6739c04ebf7602b8127c5eabe
icpereiradoprado <icpereiradoprado@gmail.com>
2022-06-02 20:00:32 -0300

    First Commit

---
+++ RsMotores.Web/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RsMotores.Web.Models;
+
+namespace RsMotores.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CarDisplay()
+        {
+            ViewBag.Id = 1;
+            ViewData["Title"] = "Oi";
+            return View();
+        }
+
+        public IActionResult CardCars()
+        {
+            return View();
+        }
+    }
+}
---
+++ RsMotores.Web/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RsMotores.Web.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Car(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+
+        public static List<Car> ShowCars()
+        {
+            List<Car> cars = new();
+            cars.Add(new Car(1, "vectra", "carro quatro portas"));
+            cars.Add(new Car(2, "Uno", "carro duas portas"));
+            cars.Add(new Car(3, "Celta", "carro duas portas"));
+            cars.Add(new Car(4, "Monza", "carro quatro portas"));
+
+            return cars;
+        }
+    }
+}
